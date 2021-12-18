42d5d5fb8b79618f8fbf10e5d00be4c5dea541bb
moon <xaria21@naver.com>
2021-12-18 17:24:24 +0900

    - Quest 관련된 서블릿,DAO,객체 추가 및 구현(임문혁)

---
+++ final_project2/src/com/controller/QuestList.java
@@ -0,0 +1,37 @@
+package com.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonArray;
+import com.model.QuestDAO;
+
+@WebServlet("/QuestList")
+public class QuestList extends HttpServlet {
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("서버진입");
+		request.setCharacterEncoding("UTF-8");
+		
+//		String id = request.getParameter("m_id");
+		String id = "bang9";
+		System.out.println("요청하는 id(안드 퀘스트부분) = "+id);
+		
+		JsonArray arr = new JsonArray();
+		
+		arr = new QuestDAO().questList(id);
+		System.out.println("보내는 객체 내용: "+arr.toString());
+		
+		
+		response.setContentType("application/json");
+	    response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(arr.toString());		
+	}
+
+}
