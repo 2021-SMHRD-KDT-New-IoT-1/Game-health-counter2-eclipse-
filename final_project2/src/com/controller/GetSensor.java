bdd33b79415746bc31970b42c0a4d5a4c2717fa2
smhrd <hanehane1103@gmail.com>
2021-12-23 09:48:42 +0900

    getSensor서블릿 라파에서 값 받고넘기기 성공

--- final_project2/src/com/controller/GetSensor.java
+++ final_project2/src/com/controller/GetSensor.java
@@ -1,6 +1,8 @@
 package com.controller;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,12 +18,17 @@ public class GetSensor extends HttpServlet {
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		String pull_cnt = "0";
-		String sqt_cnt = "0";
-		String push_cnt = "0";
-		String time_attack = "0";
-		String time_mode = "0";
+		serverLog("GetSensor");
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		String pull_cnt = "";
+		String sqt_cnt = "";
+		String push_cnt = "";
+		String time_attack = "";
+		String time_mode = "";
 		String date;
+		String result;
 		
 		// 시간까지 필요? 운동기록을 어떤 방식으로 저장할 건지.
 		// 총 개수만 뜨게 하면 된다.
@@ -31,14 +38,28 @@ public class GetSensor extends HttpServlet {
 		// 서버에 요청 보내는 시간을 제한해야한다.
 		
 		request.setCharacterEncoding("UTF-8");
-		pull_cnt = request.getParameter("");
-		sqt_cnt = request.getParameter("");
-		push_cnt = request.getParameter("");
-		date = request.getParameter("");
+		pull_cnt = request.getParameter("push_cnt");
+		sqt_cnt = request.getParameter("pull_cnt");
+		push_cnt = request.getParameter("sqt_cnt");
+		date = request.getParameter("reg_date");
 		
 		
+//		result = "pull: "+pull_cnt+", sqt: "+sqt_cnt+", push: "+push_cnt+", date: "+date;
+		result = "pull: "+pull_cnt+", sqt: "+sqt_cnt+", push: "+push_cnt+", date: "+date;
+		System.out.println(result);
+		//response.setContentType("application/json");
+	    response.setCharacterEncoding("UTF-8");
+		response.getWriter().write("성공");	
 		
-		
+	}
+	
+	public void serverLog(String serverName) {
+		Date dt = new Date();
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분 ss초");
+		String time = sdf.format(dt);
+		System.out.println();
+		System.out.println(serverName+"서버 진입(" + time + ")");
 	}
 
 }
